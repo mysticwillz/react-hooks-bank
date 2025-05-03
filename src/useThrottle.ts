e25a24400c5fdf543df6ad22df0017035986aac2
Eze <tobi@Ezes-MacBook-Pro.local>
2025-05-03 15:53:42 +0100

    add useThrottle hook

---
+++ src/useThrottle.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export function useThrottle<T>(value: T, delay: number): T {
+  const [throttledValue, setThrottledValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setThrottledValue(value), delay);
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return throttledValue;
+}
